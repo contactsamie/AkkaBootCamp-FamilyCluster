9c456e5a6accf0d002120ad2e569fb903780ce71
Sam <contactsamie@gmail.com>
2016-05-23 23:06:18 -0400

    remote into cluster

---
+++ 7-RemoteIntoCluster/FamilyCluster/FamilyCluster.FamilyFriend/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka.Actor;
+using Akka.Routing;
+using FamilyCluster.Common;
+
+namespace FamilyCluster.FamilyFriend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting FamilyFriendSyatem ...");
+            var client = ConfigurationManager.AppSettings["client"];
+            using (var system = ActorSystem.Create("FamilyFriendSyatem"))
+            {
+                while (true)
+                {
+                    var message = Console.ReadLine();
+                    var clientActor = system.ActorSelection(client);
+                    clientActor.Tell(new Hello("From FamilyFriendSyatem to client "+client+" : " + message), ActorRefs.NoSender);
+                }
+            }
+        }
+    }
+}
